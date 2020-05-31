08de8ac34fb8537e998893f921f0de47c7f86cc9
Jakub Jagosz <kuba.jagosz@gmail.com>
2020-05-31 14:45:36 +0200

    initial project setup

---
+++ src/test/kotlin/com/jjagosz/tripplanner/TripPlannerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.jjagosz.tripplanner
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TripPlannerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
